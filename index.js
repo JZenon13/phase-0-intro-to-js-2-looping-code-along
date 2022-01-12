0cd10d112b6378d327c88ec2b266dd7bab675a6e
jesse zenon <jesse.zenon@gmail.com>
2022-01-12 07:43:29 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+/*  function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); */
+
+let newArray = [];
+function writeCards(stringNames,eventName) {
+    for (let i = 0; i < stringNames.length; i++) {
+        newArray.push (`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`); 
+    debugger;}
+    return newArray;
+} 
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer--);
+    }
+}
